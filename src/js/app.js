3092280dc1e089201c50a2a4d33d541cecbe7f1c
Ben Galde <bgalde.dev@gmail.com>
2021-03-17 00:40:50 -0700

    Added bonus

--- src/js/app.js
+++ src/js/app.js
@@ -2,24 +2,82 @@ const dataFile = "/resources/data/samples.json";
 
 function buildMetadata(sampleName) {
     d3.json(dataFile).then((samples) => {
-        var panel = d3.select("#sample-metadata");
+        let panel = d3.select("#sample-metadata");
         // Clear the current metadata
         panel.html("");
 
         // Find the specific sample id in the metadata
         let sampleData = samples.metadata.find(o => o.id === +sampleName);
-        console.log(sampleData);
-
+        
         // Add each key value pair to the metadata panel
         Object.entries(sampleData).forEach(([key, value]) => {
-            panel.append("h6").text(`${key}:${value}`);
+            let h6 = panel.append("h6").style("font-size", "10px");
+            h6.append("span").text(`${key.toUpperCase()}: `).style("font-weight", "bold");
+            h6.append("span").text(`${value}`);
         });
+        
+        // build the gauge for bonus
+        buildGauge(sampleData.wfreq);
     });
+}
+
+function buildCharts(sampleName) {
+    d3.json(dataFile).then((samples) => {
+        // Find the specific sample id in the sample data
+        let sampleData = samples.samples.find(o => o.id == +sampleName);
+        
+        const otuIDs = sampleData.otu_ids;
+        const otuLabels = sampleData.otu_labels;
+        const sampleValues = sampleData.sample_values;
+
+        // Make the bubble plot
+        let bubbleLayout = {
+            margin: { t: 0 },
+            hovermode: "closests",
+            xaxis: { title: "OTU ID" },
+            yaxis: { title: "VALUES" },
+            showlegend: false
+        }
+
+        let bubbleData = [
+            {
+                x: otuIDs,
+                y: sampleValues,
+                text: otuLabels,
+                mode: "markers",
+                marker: {
+                    size: sampleValues,
+                    color: otuIDs,
+                    colorscale: "Earth"
+                }
+            }
+        ]
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+        // Build the horizontal bar chart of top ten.
+        let barData = [
+            {
+                x: sampleValues.slice(0, 10).reverse(),
+                y: otuIDs.slice(0, 10).map(d => 'OTU ' + d).reverse(),
+                hovertext: otuLabels.slice(0, 10).reverse(),
+                hoverinfo: "hovertext",
+                orientation: 'h',
+                type: "bar"
+            }
+        ];
+
+        let barLayout = {
+            margin: { t: 0, l: 80}
+        };
+
+        Plotly.newPlot("bar", barData, barLayout);
+    })
 }
 
 function init() {
     // Get the dropdown box element
-    var selector = d3.select("#selDataset");
+    let selector = d3.select("#selDataset");
 
     // Use the the names in the data to populate the dropdown
     d3.json(dataFile).then((samples) => {
@@ -32,12 +90,14 @@ function init() {
         
         // Populate the charts with the first sample name
         buildMetadata(samples.names[0]);
+        buildCharts(samples.names[0]);
     });
 }
 
 // Fetch data when the option is changed
 function optionChanged(newSample) {
-    console.log(newSample);
+    buildMetadata(newSample);
+    buildCharts(newSample);
 }
 
 // Initialize the Dashboard
